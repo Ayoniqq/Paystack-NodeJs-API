ab5054f7beacb36931a0c74b38bcf22f567575fb
Ayoniqq <ayorindeadeshina@gmail.com>
2023-03-10 22:37:25 +0100

    paystack

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 8080;
+const path = require("path");
 
 app.listen(PORT, () => {
   console.log(`LISTENING ON PORT:${PORT}`); //LISTEN
