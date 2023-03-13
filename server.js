0b3e59d64f5695ab0e0f384b420af54f7f4cc6bd
Ayoniqq <ayorindeadeshina@gmail.com>
2023-03-13 06:52:32 +0100

    paysteack

--- server.js
+++ server.js
@@ -82,5 +82,5 @@ app.get("/transaction/verify/:reference", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`LISTENING ON PORT:${PORT}`);
+  console.log(`LISTENING ON PORT:${PORT}`); //LISTEN
 });
